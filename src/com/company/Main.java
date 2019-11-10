8b02d38d3435f7b555779d7df7814f609a36cfe9
Jakub Biskup <biskup.jakub@gmail.com>
2019-11-10 17:04:03 +0100

    Add option of changing settings in the program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Scanner inScan=new Scanner(System.in);
         Settings lastSettings=new Settings();
         lastSettings.display();
+        System.out.println("Do you want to change settings? (Type in Y or N)");
+        String input=inScan.next();
+        while (!input.equalsIgnoreCase("N")){
+            if (input.equalsIgnoreCase("y")){
+                Settings.changeSettings();
+            }else{
+                System.out.println("The program only accepts Y or N answer");
+            }
+            lastSettings=new Settings();
+            lastSettings.display();
+            System.out.println("Do you want to change settings? (Type in Y or N)");
+            input=inScan.next();
+        }
         Settings settings= new Settings();
         Game game=new Game(settings);
     }
--- src/com/company/Settings.java
+++ src/com/company/Settings.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Settings {
@@ -8,12 +10,67 @@ public class Settings {
     boolean isGoingLineByLine;
     boolean isRenewingLives;
     int livesAtStart;
+    public static void changeSettings() throws Exception{
+        Scanner changeScanner=new Scanner(System.in);
+        System.out.println("Type in 1 if you have your file prepared, type in 0 if you want to generate blanks randomly");
+        String stringWithSettings="";
+        String inputQ1=changeScanner.next();
+        if (inputQ1.equals("1")){
+            stringWithSettings="1 ";
+        }
+        if (inputQ1.equals("0")){
+            stringWithSettings="0 ";
+        }
+        if (!inputQ1.equals("0")&&!inputQ1.equals("1")){
+            System.out.println("Wrong input! Only type in 1 or 0 next time!");
+            System.exit(0);
+        }
+        System.out.println("Type in 1 if you want to go one line at a time, type in 0 if you want to see the whole text every time");
+        System.out.println("Note that going one line at a time with a prepared text is a bad idea if there is no new blank every line");
+        String inputQ2=changeScanner.next();
+        if (inputQ2.equals("1")){
+            stringWithSettings+="1 ";
+        }
+        if (inputQ2.equals("0")){
+            stringWithSettings+="0 ";
+        }
+        if (!inputQ2.equals("0")&&!inputQ2.equals("1")){
+            System.out.println("Wrong input! Only type in 1 or 0 next time!");
+            System.exit(0);
+        }
+        System.out.println("Type in 1 to renew lives after a correct guess, type in 0 if you don't want to renew lives");
+        String inputQ3=changeScanner.next();
+        if (inputQ3.equals("1")){
+            stringWithSettings+="1 ";
+        }
+        if (inputQ3.equals("0")){
+            stringWithSettings+="0 ";
+        }
+        if (!inputQ3.equals("0")&&!inputQ3.equals("1")){
+            System.out.println("Wrong input! Only type in 1 or 0 next time!");
+            System.exit(0);
+        }
+        System.out.println("Type in how many guesses you want to get at start (positive integer value)");
+        try{
+            int inputQ4=changeScanner.nextInt();
+            //accepts negative values (user will loose before seeing the blanks)
+            stringWithSettings+=Integer.toString(inputQ4);
+        }catch (Exception e){
+            System.out.println("Wrong input! Type in an integer next time!");
+            System.exit(0);
+        }
+        FileWriter fw = new FileWriter("settings.txt");
+        PrintWriter pw = new PrintWriter(fw);
+
+        pw.println(stringWithSettings);
+        pw.close();
+    }
     public void display(){
         System.out.println("Current settings are:");
         if(this.isPrepped){
             System.out.println("-Prepared files");
         }else {
-            System.out.println("-Random blanks");
+            System.out.println("-Random blanks (one per line)");
         }
         if(this.isGoingLineByLine){
             System.out.println("-Displaying single line");
@@ -22,7 +79,7 @@ public class Settings {
         }
         System.out.println("-Guesses at start: "+this.livesAtStart);
         if(this.isRenewingLives){
-            System.out.println("-Restoring available guesses after correct guess");
+            System.out.println("-Restoring guesses after correct guess");
         }else {
             System.out.println("-Guesses are never restored");
         }
